0edf86bf8d8a72a4d2a49bbb2bd47daa7a5931a7
Romario lima <romariolima1006@gmail.com>
2025-01-29 08:50:19 -0300

    home view e url

--- curso/recipes/views.py
+++ curso/recipes/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def home(request):
+    context= {'ola': 'mundo'}
+    return render(request, 'recipes/home.html', context=context)
+
+
+def contato(request):
+    return HttpResponse('contato')
+
+
+def sobre(request):
+    return HttpResponse('sobre')
